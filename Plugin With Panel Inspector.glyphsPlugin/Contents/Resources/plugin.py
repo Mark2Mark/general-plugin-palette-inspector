b8e0b5b6f358bafbf5df3430db9a69509eeea464
Mark Frömberg <mirque84@gmail.com>
2023-10-14 19:01:02 +0200

    clean up a little

--- Plugin With Panel Inspector.glyphsPlugin/Contents/Resources/plugin.py
+++ Plugin With Panel Inspector.glyphsPlugin/Contents/Resources/plugin.py
@@ -15,6 +15,16 @@ from __future__ import division, print_function, unicode_literals
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
+from vanilla import (
+    Window,
+    Group,
+    TextBox,
+)
+
+
+# Our own patched Vanilla Group class
+class PatchedGroup(Group):
+    nsViewClass = objc.lookUpClass("GSInspectorView")
 
 
 class MyPluginWithPanelInspector(GeneralPlugin):
@@ -27,12 +37,47 @@ class MyPluginWithPanelInspector(GeneralPlugin):
             }
         )
 
+        self.width = 150
+        self.height = 180
+        self.paletteView = Window((self.width, self.height))
+        self.paletteView.group = PatchedGroup(
+            (0, 0, self.width, self.height)
+        )  # Using PatchedGroup() here instead of Group() (for the inspecotr view)
+        self.paletteView.group.label = TextBox(("auto"), "Hello Panel Inspector")
+
+        rules = [
+            "H:|-10-[label]-10-|",
+            "V:|-5-[label]-5-|",
+        ]
+        self.paletteView.group.addAutoPosSizeRules(rules)
+
+        # GSCallbackHandler.addCallback_forOperation_(
+        #     self, "GSInspectorViewControllersCallback"
+        # )
+        GSCallbackHandler.addCallback_forOperation_(
+            self,
+            "GSPanelInspectorsViewControllersCallbackName",
+        )
+
     @objc.python_method
     def start(self):
-        print("started Plugin with Panel Inspector")
+        pass
 
-        # newMenuItem = NSMenuItem(self.name, self.showWindow_)
-        # Glyphs.menu[EDIT_MENU].append(newMenuItem)
+    # def inspectorViewControllersForLayer_(self, layer):
+    def panelInspectorsForLayer_(self, layer):
+        try:
+            self.paletteView.group.label.set(layer.name)
+        except:
+            pass
+        if layer and layer.selection:
+            self.paletteView.group.label.set(
+                [s.__class__.__name__ for s in layer.selection]
+            )
+            return [self]
+        return [self]
+
+    def view(self):
+        return self.paletteView.group.getNSView()
 
     @objc.python_method
     def __file__(self):
